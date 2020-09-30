a5dc946accc6e0c3b929fffbe82b3d7f389ba7ae
Gulshan <gulshantaneja1@gmail.com>
2020-09-30 13:34:06 +0530

    Added Client Code

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
-    [Authorize]
+
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -31,8 +31,7 @@ namespace DatingApp.API.Controllers
         }
 
         // GET api/values/5
-        [HttpGet("{id}")]
-        [AllowAnonymous]
+        [HttpGet("{id}")]      
         public async Task<IActionResult> GetValue(int id)
         {
             var value = await _context.Values.FirstOrDefaultAsync( item => item.Id == id);
