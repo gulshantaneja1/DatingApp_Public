2965c5a1beb9aa6d6cf7c3d8aec3f6304e58aa64
Gulshan <gulshantaneja1@gmail.com>
2020-09-30 18:49:20 +0530

    Added login Component

---
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss'],
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+
+  constructor(private authServices: AuthService) {}
+
+  ngOnInit() {}
+
+  login() {
+    this.authServices.login(this.model).subscribe(
+      (response) => {
+        console.log('Logged in Successfully');
+      },
+      (error) => {
+        console.log('Failed to login');
+      }
+    );
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('Logged out');
+  }
+
+  
+}
